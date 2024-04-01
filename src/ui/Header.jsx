bca3a4930f7882c5fa938cfa38d6585e1d7ef085
Harish <khharish2@gmail.com>
2024-04-01 21:34:29 +0100

    changes

---
+++ src/ui/Header.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+
+    <header>
+        <Link to="/">Fast React Pizza Co.</Link>
+
+        <p>Header</p>
+    </header>
+  )
+}
+
+export default Header
